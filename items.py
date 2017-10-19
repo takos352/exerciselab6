38c21b5c5bd6119f0391a36618e99ebdb6dca624
takos352 <savvasga352@gmail.com>
2017-10-19 20:20:07 +0100

    ...

---
+++ items.py
@@ -0,0 +1,69 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+    
+    "mass":50
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!",
+    "mass":800
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees.",
+
+    "mass":200
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits.",
+    "mass":300
+}
+
+item_pen = {
+    "id": "pen",
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen.",
+    "mass":100
+
+}
+item_handbook = {
+    "id": "handbook",
+    
+    "name": "a student handbook",
+
+    "desription": "a student handbook",
+
+    "mass":50
+
+}
+items ={
+    "id":item_id,
+    "laptop":item_laptop,
+    "money":item_money,
+    "biscuits":item_biscuits,
+    "pen":item_pen,
+    "handbook":item_handbook
+}
---
+++ player.py
@@ -0,0 +1,8 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+
+# Start game at the reception
+current_room = rooms["Reception"]
+current_mass = 0
